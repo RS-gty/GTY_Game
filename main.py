0334b561953e30afc0cd96c46628bc6656683f61
RS-gty <18920702372@163.com>
2022-09-01 00:16:18 +0800

    Change

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+s = input()
+t = ''
+for c in s:
+    if 'a' <= c <= 'z':
+        t += chr(ord('a') + ((ord(c) - ord('a')) + 3) % 26)
+    elif 'A' <= c <= 'Z':
+        t += chr(ord('A') + ((ord(c) - ord('A')) + 3) % 26)
+    else:
+        t += c  # t+=是实现了两个字符相加，eg：输入abc第一次循环t = d，第二次循环t = de，第三次循环t = def
+print(t)
