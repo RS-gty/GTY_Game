ef325749e17914269888a76e553ef1cb653d670f
RS-gty <18920702372@163.com>
2022-09-01 14:22:09 +0800

    Change

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 
-cmd = 'functions.exe aha'
-a = os.system(cmd)
-print(a)
+sentence = 'This/is/a/sentence/for/testing'
+password = 't'
+
+cmd1 = 'encode.exe' + ' ' + sentence + ' ' + password
+aa = os.system(cmd1)
+cmd2 = 'decode.exe' + ' ' + password
+a = os.system(cmd2)
