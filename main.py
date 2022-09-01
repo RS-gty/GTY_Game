5e6bc5b22c5890ce0dd91a44a4fa2c1a9040c5b0
RS-gty <18920702372@163.com>
2022-09-01 08:39:25 +0800

    Change

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-s = input()
-t = ''
-for c in s:
-    if 'a' <= c <= 'z':
-        t += chr(ord('a') + ((ord(c) - ord('a')) + 3) % 26)
-    elif 'A' <= c <= 'Z':
-        t += chr(ord('A') + ((ord(c) - ord('A')) + 3) % 26)
-    else:
-        t += c  # t+=是实现了两个字符相加，eg：输入abc第一次循环t = d，第二次循环t = de，第三次循环t = def
-print(t)
+import os
+
+cmd = 'functions.exe aha'
+a = os.system(cmd)
+print(a)
