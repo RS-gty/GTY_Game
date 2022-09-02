82de01e153099295c9e6175b93075024b5e67fa7
RS-gty <18920702372@163.com>
2022-09-02 09:55:24 +0800

    Change

--- functions.py
+++ functions.py
@@ -1,11 +1,50 @@
-import os
+"""
+A function package for encoding and decoding of strings
+"""
 
 
-def encode(string: str, password: str) -> None:
-    cmd = 'encode.exe' + ' ' + string + ' ' + password
-    c = os.system(cmd)
+def encode(string: str, password: str):
+    password1 = 1
+    if len(password) > 1:
+        for i in password:
+            password1 *= ord(i)
+    else:
+        password1 = ord(password)
+    output = ''
+    out = ''
+    for i in string:
+        if len(str(ord(i) + password1)) > 2:
+            output += str(ord(i) + password1)[::-1]
+            out += str(ord(i) + password1)[::-1] + '|'
+        else:
+            output += ('0' + str(ord(i) + password1))[::-1]
+            out += ('0' + str(ord(i) + password1))[::-1] + '|'
+    return int(output + str(len(out.split('|')[0])))
 
 
-def decode(password: str) -> None:
-    cmd = 'decode.exe' + ' ' + password
-    c = os.system(cmd)
+def decode(integer: int, password: str):
+    password1 = 1
+    if len(password) > 1:
+        for i in password:
+            password1 *= ord(i)
+    else:
+        password1 = ord(password)
+    r = str(integer)[0:-1]
+    n = 1
+    m = ''
+    l1 = []
+    l2 = []
+    output = ''
+    for i in str(integer):
+        if n % int(str(integer)[-1]) == 0:
+            m += i
+            l1.append(m)
+            m = ''
+        else:
+            m += i
+        n += 1
+    for i in l1:
+        l2.append(int(i[::-1]) - password1)
+    for i in l2:
+        output += chr(i)
+    return output
--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import os
+# import time
+import pygame
+import sys
 
-sentence = 'This/is/a/sentence/for/testing'
-password = 't'
 
-cmd1 = 'encode.exe' + ' ' + sentence + ' ' + password
-aa = os.system(cmd1)
-cmd2 = 'decode.exe' + ' ' + password
-a = os.system(cmd2)
+class Player(object):
+    def __init__(self):
+        self.position = [0, 0]
+        self.speed = 1
+        self.inventory = []
+
+
+def submit():
+    print(114514)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 450))
+    pygame.display.set_caption('GTY-game')
+    icon = pygame.image.load('/data/icon.ico')
+    pygame.display.set_icon(icon)
+
+    surface_image = pygame.image.load("/data/player.png")
+    image_rect = surface_image.get_rect()
+    image_rect.center = (100, 100)
+    screen.blit(surface_image, image_rect)
+
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
